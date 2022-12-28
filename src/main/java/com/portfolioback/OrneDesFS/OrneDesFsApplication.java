026ead6652a1a0a278a858f1d4e2e436d840b7d3
OrneDFS <ornella.desarrolladorafs@gmail.com>
2022-12-27 23:57:31 -0300

    Creación del proyecto en SpringBoot: OrneDesFS

---
+++ src/main/java/com/portfolioback/OrneDesFS/OrneDesFsApplication.java
@@ -0,0 +1,14 @@
+package com.portfolioback.OrneDesFS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrneDesFsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrneDesFsApplication.class, args);
+                System.out.println("HOLA FUCKING WORLD");
+	}
+
+}
